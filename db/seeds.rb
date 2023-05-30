83a333cc9ea3b4b9ca65f1fda83f9473a72d3776
Jan Plewa <jan.plewa@web.de>
2023-05-30 17:36:30 +0200

    Adjusted the seed, so it automatically creates 2 Users with instruments and bookings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'date'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +6,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "creating Bach and Vivaldi users"
+User.create(
+  email: "sebastian.bach@web.de",
+  password: "sebastian.bach@web.de"
+)
+User.create(
+  email: "antonio.vivaldi@web.de",
+  password: "password"
+)
+
+p "created user valid? = #{User.last.valid?}"
+
+puts "creating Bach's Kirchenorgel"
+Instrument.create(
+  name: "Kirchenorgel",
+  user: User.find(1),
+  instrument_type: "organ",
+  price_per_day: 50,
+  description: "Perfect for playing the Toccata and Fugue in D minor, BWV 565!"
+)
+p "created instrument valid? = #{Instrument.last.valid?}"
+
+puts "creating Vivaldi's Miglior Violino"
+Instrument.create(
+  name: "Miglior Violino",
+  user: User.find(2),
+  instrument_type: "viola",
+  price_per_day: 50,
+  description: "Perfect for playing the four seasons!"
+)
+p "created instrument valid? = #{Instrument.last.valid?}"
 
 puts "creating 20 Fake instruments"
 list_of_instruments = %w(piano violin cello viola flute guitar clarinette oboe trombone drum organ)
@@ -17,3 +49,51 @@ list_of_instruments = %w(piano violin cello viola flute guitar clarinette oboe t
     description: "What a nice instrument!"
   )
 end
+
+p "created instrument valid? = #{Instrument.last.valid?}"
+
+puts "creating a fake pending booking for Vivaldi"
+Booking.create(
+  rent_date: Date.today,
+  user_id: User.find(2),
+  instrument_id: Instrument.find(1)
+)
+
+puts "creating a fake accepted booking for Vivaldi"
+Booking.create(
+  rent_date: Date.yesterday,
+  user_id: User.find(2),
+  instrument_id: Instrument.find(1),
+  status: "accepted"
+)
+
+puts "creating a fake rejected booking for Vivaldi"
+Booking.create(
+  rent_date: Date.yesterday,
+  user_id: User.find(2),
+  instrument_id: Instrument.find(1),
+  status: "rejected"
+)
+
+puts "creating a fake pending booking for Bach"
+Booking.create(
+  rent_date: Date.today,
+  user_id: User.find(1),
+  instrument_id: Instrument.find(2)
+)
+
+puts "creating a fake accepted booking for Bach"
+Booking.create(
+  rent_date: Date.yesterday,
+  user_id: User.find(1),
+  instrument_id: Instrument.find(2),
+  status: "accepted"
+)
+
+puts "creating a fake rejected booking for Bach"
+Booking.create(
+  rent_date: Date.yesterday,
+  user_id: User.find(1),
+  instrument_id: Instrument.find(2),
+  status: "rejected"
+)
