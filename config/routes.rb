274fb90f1f4fd2cb92340b3463bfb6b6f8429b5e
Jan Plewa <jan.plewa@web.de>
2023-05-30 14:37:08 +0200

    Bookings can now be created and are linked to via the instrument_show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :instruments, only: [:show, :new, :create] do
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:new, :create]
   end
 end
