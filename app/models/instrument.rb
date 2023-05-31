671d8eecbf7e654fd19e8e3463c8d85b005f6180
Mago Arcera <magocali44@gmail.com>
2023-05-31 14:22:56 +0200

    notifications

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,7 @@
 class Instrument < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  has_many :bookings
   # validations
   validates :name, presence: true
   validates :instrument_type, presence: true
