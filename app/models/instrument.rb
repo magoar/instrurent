70f4d86a06584823909ac33f181b4dfd91063532
Jan Plewa <jan.plewa@web.de>
2023-06-01 12:58:28 +0200

    Implemented the Search

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -6,4 +6,14 @@ class Instrument < ApplicationRecord
   validates :name, presence: true
   validates :instrument_type, presence: true
   validates :description, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: [ :name, :instrument_type ],
+                  associated_against: {
+                    user: [ :first_name, :last_name ]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
