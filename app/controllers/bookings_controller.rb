5d39d3c43977ead09fdb2f84c83ec946af63416d
Jan Plewa <jan.plewa@web.de>
2023-06-02 16:34:25 +0200

    fixed seed and notification

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,19 +5,21 @@ class BookingsController < ApplicationController
     @booking.instrument = @instrument
     @booking.user = current_user
     @booking.save
-    flash[:notice] = 'You successfully Booked the instrument!'
+    flash[:notice] = 'You successfully booked the instrument!'
     redirect_to dashboard_path
   end
 
   def update
     @booking = Booking.find(params[:id])
     @booking.update(status: "accepted")
+    flash[:notice] = "You accepted the request of #{@booking.user}!"
     redirect_to instrument_path(flag: "dasboard")
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
+    flash[:notice] = 'You successfully deleted your instrument!'
     redirect_to dashboard_path(flag: "dasboard"), status: :see_other
   end
 
